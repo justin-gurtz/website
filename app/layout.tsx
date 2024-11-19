1fc01f20aa8dd8b8eabfbc4a78726a3fe910c67f
Justin Gurtz <40037115+justin-gurtz@users.noreply.github.com>
2024-11-19 06:37:06 -0500

    Switch to src folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,13 @@ import localFont from 'next/font/local'
 import '@/globals.css'
 
 const geistSans = localFont({
-  src: '../fonts/GeistVF.woff',
+  src: '../src/fonts/GeistVF.woff',
   variable: '--font-geist-sans',
   weight: '100 200 300 400 500 600 700 800 900',
 })
 
 const geistMono = localFont({
-  src: '../fonts/GeistMonoVF.woff',
+  src: '../src/fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
   weight: '100 200 300 400 500 600 700 800 900',
 })
