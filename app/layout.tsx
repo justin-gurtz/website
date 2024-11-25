a5fbd94831301a4fa4c9373cc5ea6faa7fac13e3
Justin Gurtz <40037115+justin-gurtz@users.noreply.github.com>
2024-11-24 22:09:13 -0500

    Fix Geist render issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
 import '@/globals.css'
 import { Analytics } from '@vercel/analytics/next'
-
-const geistSans = localFont({
-  src: '../src/fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 200 300 400 500 600 700 800 900',
-})
-
-const geistMono = localFont({
-  src: '../src/fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 200 300 400 500 600 700 800 900',
-})
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 
 export const metadata: Metadata = {
   title: 'Justin Gurtz',
@@ -28,7 +17,7 @@ const RootLayout = ({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-neutral-900`}
+        className={`${GeistSans.variable} ${GeistMono.variable} antialiased dark:bg-neutral-900`}
       >
         {children}
         <Analytics />
