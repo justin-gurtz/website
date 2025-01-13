b5f71254e7cec2c2f12057effb45a29d15b3947a
Justin Gurtz <40037115+justin-gurtz@users.noreply.github.com>
2025-01-12 20:07:21 -0500

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next'
 import '@/globals.css'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
+
+const bodyClassName = `${GeistSans.variable} ${GeistMono.variable} antialiased font-sans text-neutral-800 dark:text-white bg-neutral-100 dark:bg-neutral-800`
 
 export const metadata: Metadata = {
   title: 'Justin Gurtz',
@@ -13,17 +16,14 @@ const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode
-}>) => {
-  return (
-    <html lang="en">
-      <body
-        className={`${GeistSans.variable} ${GeistMono.variable} antialiased font-sans text-neutral-800 dark:text-white bg-neutral-100 dark:bg-neutral-800`}
-      >
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  )
-}
+}>) => (
+  <html lang="en">
+    <body className={bodyClassName}>
+      {children}
+      <Analytics />
+      <SpeedInsights />
+    </body>
+  </html>
+)
 
 export default RootLayout
