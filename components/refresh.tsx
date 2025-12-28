fd86edd9c1783d385f1015b93eccf99a43b8ea63
Justin Gurtz <40037115+justin-gurtz@users.noreply.github.com>
2025-12-28 08:55:44 -0500

    Stop refreshing if offline

--- components/refresh.tsx
+++ components/refresh.tsx
@@ -2,14 +2,18 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useRef } from "react";
+import useIsOnline from "@/hooks/use-is-online";
 import usePageIsVisible from "@/hooks/use-page-is-visible";
 
 const Refresh = ({ every }: { every: number }) => {
   const { refresh } = useRouter();
+
+  const isOnline = useIsOnline();
   const pageIsVisible = usePageIsVisible();
   const isInitialMount = useRef(true);
 
   useEffect(() => {
+    if (!isOnline) return;
     if (!pageIsVisible) return;
 
     if (isInitialMount.current) {
@@ -21,7 +25,7 @@ const Refresh = ({ every }: { every: number }) => {
     const interval = setInterval(refresh, every * 1000);
 
     return () => clearInterval(interval);
-  }, [every, refresh, pageIsVisible]);
+  }, [every, refresh, pageIsVisible, isOnline]);
 
   return null;
 };
